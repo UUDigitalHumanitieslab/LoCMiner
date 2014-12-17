05bd51e5213401e491eef8cc72a3b9d2adc40aeb
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2014-12-17 13:57:23 +0100

    Updated Celery config

--- LoCMiner/config.py
+++ LoCMiner/config.py
@@ -6,8 +6,9 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://loc:locminer@localhost:5432/loc'
-    CELERY_BROKER_URL = 'redis://localhost:6379'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+    CELERY_BROKER_URL = 'redis://localhost:6379/1'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+    CELERY_DEFAULT_QUEUE = 'loc'
     SECRET_KEY = 'development key'
 
 
