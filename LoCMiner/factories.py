c94e29d75544aa763ee220ea046539b567cc5c39
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-12-10 14:09:57 +0100

    Explicitly setting the Celery backend

--- LoCMiner/factories.py
+++ LoCMiner/factories.py
@@ -20,7 +20,9 @@ def create_app(config):
 
 def create_celery(app=None):
     app = app or create_app(ProductionConfig)
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery = Celery(app.import_name,
+                    backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
 
